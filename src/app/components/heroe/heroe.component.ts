de5361cde5daa32787fc683c3196628e82e0daee
jose hernandez <jose2889@gmail.com>
2018-05-29 00:47:53 -0400

    primera parte de la practica 2 spa

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: []
+})
+export class HeroeComponent implements OnInit {
+
+  heroe:any;
+
+  constructor(private activatedRoute:ActivatedRoute, private heroesService:HeroesService) {
+
+    this.activatedRoute.params.subscribe( p => {
+      this.heroe = this.heroesService.getHeroe(p['id']);
+    });
+   }
+
+  ngOnInit() {
+  }
+
+}
